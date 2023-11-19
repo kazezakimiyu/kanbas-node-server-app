38dce58e6a6556e9e4668d9af4190f572e0c14ff
Ruixin Li <kazezakimiyu@gmail.com>
2023-11-18 19:06:04 -0500

    delete api in url

--- courses/routes.js
+++ courses/routes.js
@@ -1,23 +1,23 @@
 import Database from "../Database/index.js";
 function CourseRoutes(app) {
-    app.post("/api/courses", (req, res) => {
+    app.post("/courses", (req, res) => {
         const course = { ...req.body,
             _id: new Date().getTime().toString() };
         Database.courses.push(course);
         res.send(course);
     });
     
-    app.get("/api/courses", (req, res) => {
+    app.get("/courses", (req, res) => {
         const courses = Database.courses;
         res.send(courses);
     });
-    app.delete("/api/courses/:id", (req, res) => {
+    app.delete("/courses/:id", (req, res) => {
         const { id } = req.params;
         Database.courses = Database.courses
           .filter((c) => c._id !== id);
         res.sendStatus(204);
     });
-    app.put("/api/courses/:id", (req, res) => {
+    app.put("/courses/:id", (req, res) => {
         const { id } = req.params;
         const course = req.body;
         Database.courses = Database.courses.map((c) =>
@@ -25,7 +25,7 @@ function CourseRoutes(app) {
         );
         res.sendStatus(204);
     });
-    app.get("/api/courses/:id", (req, res) => {
+    app.get("/courses/:id", (req, res) => {
         const { id } = req.params;
         const course = Database.courses
           .find((c) => c._id === id);
--- modules/routes.js
+++ modules/routes.js
@@ -1,12 +1,12 @@
 import db from "../Database/index.js";
 function ModuleRoutes(app) {
-    app.get("/api/courses/:cid/modules", (req, res) => {
+    app.get("/courses/:cid/modules", (req, res) => {
         const { cid } = req.params;
         const modules = db.modules
         .filter((m) => m.course === cid);
         res.send(modules);
     });
-    app.post("/api/courses/:cid/modules", (req, res) => {
+    app.post("/courses/:cid/modules", (req, res) => {
         const { cid } = req.params;
         const newModule = {
           ...req.body,
@@ -16,12 +16,12 @@ function ModuleRoutes(app) {
         db.modules.push(newModule);
         res.send(newModule);
     });
-    app.delete("/api/modules/:mid", (req, res) => {
+    app.delete("/modules/:mid", (req, res) => {
         const { mid } = req.params;
         db.modules = db.modules.filter((m) => m._id !== mid);
         res.sendStatus(200);
     });
-    app.put("/api/modules/:mid", (req, res) => {
+    app.put("/modules/:mid", (req, res) => {
         const { mid } = req.params;
         const moduleIndex = db.modules.findIndex(
           (m) => m._id === mid);
