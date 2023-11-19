7c2e4839034eb2b543e7ef790980a3677929c9bd
Ruixin Li <83576311+kazezakimiyu@users.noreply.github.com>
2023-11-18 20:18:54 -0500

    Update index.js

--- Database/index.js
+++ Database/index.js
@@ -1,10 +1,11 @@
-import courses from "./courses.json" assert { type: "json" };
-import modules from "./modules.json" assert { type: "json" };
-import assignments from "./assignments.json" assert { type: "json" };
-import users from "./users.json" assert { type: "json" };
-import grades from "./grades.json" assert { type: "json" };
-import enrollments from "./enrollments.json" assert { type: "json" };
-export default {
+const courses = require('./courses.json');
+const modules = require('./modules.json');
+const assignments = require('./assignments.json');
+const users = require('./users.json');
+const grades = require('./grades.json');
+const enrollments = require('./enrollments.json');
+
+module.exports = {
     courses,
     modules,
     assignments,
